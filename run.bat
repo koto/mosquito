321fba62afcffe738fe1f143b811efbb75071106
Krzysztof Kotowicz <kkotowicz@gmail.com>
2013-07-14 21:10:06 +0200

    changed run scripts to run http-server.py

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 rem start whole mosquito toolkit in windows
 rem requires python on path
 
-start cmd /k "cd webroot && python -m SimpleHTTPServer"
+start cmd /k "python http-server.py webroot 8000"
 start cmd /k "cd externals\MalaRIA-Proxy\proxy-backend & java malaria.MalariaServer dummy 8081 4444"
 start cmd /k "cd externals\websockify-exe & websockify.exe 8082 localhost:8081"
 start http://localhost:8000/generate.html
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # start whole mosquito toolkit in Ubuntu based systems
-gnome-terminal -t SimpleHTTPServer -x sh -c "cd webroot && python -m SimpleHTTPServer"
+gnome-terminal -t SimpleHTTPServer -x sh -c "python ./http-server.py webroot/ 8000"
 gnome-terminal -t MalaRIA -x sh -c "cd externals/MalaRIA-Proxy/proxy-backend && java malaria.MalariaServer dummy 8081 4444"
 gnome-terminal -t websockify -x sh -c "cd externals/websockify && python ./websockify.py 8082 localhost:8081"
 xdg-open http://localhost:8000/generate.html
